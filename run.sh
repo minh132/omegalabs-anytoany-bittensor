7aa18383f888ec95ceccba324fefd7fb012b48bc
minh132 <89315105+minh132@users.noreply.github.com>
2024-08-26 10:59:46 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 pip install -e .
+huggingface-cli login --token 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
 make download-everything
 git clone https://huggingface.co/impossibleexchange/try2takemyhat
 make finetune-x1
