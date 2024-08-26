2778fcb663959c6067a48541e0bbda6b9aeadb5c
minh132 <89315105+minh132@users.noreply.github.com>
2024-08-26 12:36:04 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ huggingface-cli login --token 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
 make download-everything
 git clone https://huggingface.co/impossibleexchange/try2takemyhat
 make finetune-x1
-python miner_utils/up.py --hf_repo_id minh132/test --model_dir output_checkpoints/experiment_1/ --epoch 0
-
+export HF_ACCESS_TOKEN=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
+python miner_utils/up.py --hf_repo_id cmncomp/a2a_vp --model_dir output_checkpoints/experiment_1/ --epoch 0 
